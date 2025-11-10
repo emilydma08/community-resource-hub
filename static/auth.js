98e17c59b153b3f5619304a59720deb79949d987
r-i-y-aa <ri.kulkarni02@gmail.com>
2025-11-10 01:29:51 -0800

    set up registration logic

--- static/auth.js
+++ static/auth.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-analytics.js";
+  import { createUserWithEmailAndPassword, getAuth } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-analytics.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,3 +20,22 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebas
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
+  const submit = document.getElementById('submit');
+  submit.addEventListener('click', function(event) {
+    event.preventDefault();
+    const auth = getAuth();
+    const username = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+  });
