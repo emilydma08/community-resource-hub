b9179981562cc3cdca5a85eaa4acb1ea711d5a2e
r-i-y-aa <ri.kulkarni02@gmail.com>
2025-11-11 13:33:23 -0800

    finished registration logic

--- static/auth.js
+++ static/auth.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
-  import { createUserWithEmailAndPassword, getAuth } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-analytics.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
+import { createUserWithEmailAndPassword, getAuth } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
@@ -23,17 +23,45 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebas
   const submit = document.getElementById('submit');
   submit.addEventListener('click', function(event) {
     event.preventDefault();
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
     const auth = getAuth();
-    const username = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+    const email = emailInput.value;
+  const password = passwordInput.value;
   createUserWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
     // Signed up 
     const user = userCredential.user;
+    window.location.href = '/dashboard'
     // ...
   })
   .catch((error) => {
     const errorCode = error.code;
+    if (errorCode == 'auth/email-already-in-use') {
+      alert('Email is taken');
+      emailInput.value = '';
+      passwordInput.value = '';
+    }
+    else if (errorCode == 'auth/invalid-email') {
+      alert('Not a valid email');
+      emailInput.value = '';
+      passwordInput.value = '';
+    }
+    else if (errorCode == 'auth/weak-password') {
+      alert('Password is too short');
+      emailInput.value = '';
+      passwordInput.value = '';
+    }
+    else if (errorCode == 'auth/missing-email') {
+      alert('Please enter a valid email');
+      emailInput.value = '';
+      passwordInput.value = '';
+    }
+    else {
+      alert('Something went wrong. Please try again later');
+      emailInput.value = '';
+      passwordInput.value = '';
+    }
     const errorMessage = error.message;
     // ..
   });
