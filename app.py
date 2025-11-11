9adcf70b5974d501bb90bc416e0dec2f8e0632b1
r-i-y-aa <ri.kulkarni02@gmail.com>
2025-11-11 13:33:23 -0800

    finished registration logic

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def landing():
-    return render_template('index.html')
+    return render_template('register.html')
 
 @app.route('/login', methods=['GET'])
 def login():
     return render_template('login.html')
 
-@app.route('/register', methods=['GET'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     return render_template('register.html')
 
