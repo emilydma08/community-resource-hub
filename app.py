2554328d4c939d6937b675436ddd1ecbe0890ae7
Emily Ma <emilyma@outlook.com>
2026-01-22 07:36:40 -0800

    port fixes

--- app.py
+++ app.py
@@ -131,5 +131,6 @@ def delete_form():
     return redirect(url_for('my_orgs'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
     
